8c9c7af2f93ddc2ffac0dac775ed010e56101dc2
Dan Fellner <dfellner92@gmail.com>
2021-05-15 18:14:48 -0400

    adjusted3

---
+++ hope/src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg shadow-lg p-3 mb-5 bg-white rounded">
+        <a className="navbar-brand" href="">
+          Dan's Pizza
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav m1-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="">
+                Login
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Cart
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
